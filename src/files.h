4b5dac11ab9e7e2bf324afb04825ae92938f8780
Thorsten Kukuk <kukuk@suse.com>
2025-10-15 11:56:32 +0200

    Add get_local_user_record() function

--- src/files.h
+++ src/files.h
@@ -4,6 +4,9 @@
 
 #include <pwd.h>
 #include <shadow.h>
+#include <security/pam_modules.h>
 
+extern int get_local_user_record(pam_handle_t *pamh, const char *user,
+		struct passwd **ret_pw, struct spwd **ret_sp);
 extern int update_passwd(struct passwd *newpw, const char *etcdir);
 
