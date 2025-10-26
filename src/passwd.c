7d07d0986d68a9d5fd25ea9b41a8109f4cd0eab3
Thorsten Kukuk <kukuk@suse.com>
2025-10-26 09:13:24 +0100

    passwd: implement delete password

--- src/passwd.c
+++ src/passwd.c
@@ -15,6 +15,8 @@
 
 #define USEC_INFINITY ((uint64_t) UINT64_MAX)
 
+#define ARG_DELETE_PASSWORD 1
+
 static void
 print_usage(FILE *stream)
 {
@@ -28,6 +30,7 @@ print_help(void)
 
   print_usage(stdout);
 
+  fputs("  -d, --delete      Delete password\n", stdout);
   fputs("  -k, --keep-tokens Change only expired passwords\n", stdout);
   fputs("  -h, --help        Give this help list\n", stdout);
   fputs("  -v, --version     Print program version\n", stdout);
@@ -39,11 +42,162 @@ print_error(void)
   fprintf (stderr, "Try `passwd --help' or `passwd --usage' for more information.\n");
 }
 
+static int
+modify_account(const char *user, int args)
+{
+  _cleanup_(sd_varlink_unrefp) sd_varlink *link = NULL;
+  _cleanup_(sd_json_variant_unrefp) sd_json_variant *passwd = NULL;
+  _cleanup_(sd_json_variant_unrefp) sd_json_variant *shadow = NULL;
+  _cleanup_(sd_json_variant_unrefp) sd_json_variant *params = NULL;
+  _cleanup_(sd_json_variant_unrefp) sd_json_variant *result = NULL;
+  _cleanup_free_ char *error = NULL;
+  _cleanup_(struct_passwd_freep) struct passwd *pw = NULL;
+  _cleanup_(struct_shadow_freep) struct spwd *sp = NULL;
+  bool complete = false;
+  // XXX _cleanup_(struct_result_free) struct result p = {
+  struct result p = {
+    .success = false,
+    .error = NULL,
+  };
+  static const sd_json_dispatch_field dispatch_table[] = {
+    { "Success",  SD_JSON_VARIANT_BOOLEAN, sd_json_dispatch_stdbool, offsetof(struct result, success), 0 },
+    { "ErrorMsg", SD_JSON_VARIANT_STRING,  sd_json_dispatch_string,  offsetof(struct result, error), 0 },
+    {}
+  };
+  int r;
+
+  r = pwaccess_get_user_record(-1, user, &pw, &sp, &complete, &error);
+  if (r < 0)
+    {
+      if (error)
+        fprintf (stderr, "%s\n", error);
+      else
+        fprintf (stderr, "get_user_record failed: %s\n", strerror(-r));
+      return -r;
+    }
+
+  if (pw == NULL)
+    {
+      fprintf(stderr, "ERROR: no password entry found!\n");
+      return 1; /* XXX map to errno */
+    }
+
+  if (!complete)
+    printf("Warning: couldn't read password field, ignoring.\n");
+
+  if (args & ARG_DELETE_PASSWORD)
+    {
+      if (pw->pw_passwd)
+	pw->pw_passwd = mfree(pw->pw_passwd);
+
+      pw->pw_passwd = strdup("");
+      if (pw->pw_passwd == NULL)
+	{
+	  fprintf(stderr, "Out of memory!\n");
+	  return -ENOMEM;
+	}
+
+      if (sp)
+	{
+	  if (sp->sp_pwdp)
+	    sp->sp_pwdp = mfree(sp->sp_pwdp);
+
+	  sp->sp_pwdp = strdup("");
+	  if (sp->sp_pwdp == NULL)
+	    {
+	      fprintf(stderr, "Out of memory!\n");
+	      return -ENOMEM;
+	    }
+	}
+    }
+  /* XXX unify opening socket */
+  r = connect_to_pwupdd(&link, _VARLINK_PWUPD_SOCKET, &error);
+  if (r < 0)
+    {
+      if (error)
+	fprintf(stderr, "%s\n", error);
+      else
+	fprintf(stderr, "Cannot connect to pwupd! (%s)\n", strerror(-r));
+      return -r;
+    }
+
+  r = sd_json_variant_merge_objectbo(&passwd,
+                                     SD_JSON_BUILD_PAIR_STRING("name", pw->pw_name),
+                                     SD_JSON_BUILD_PAIR_STRING("passwd", pw->pw_passwd),
+                                     SD_JSON_BUILD_PAIR_INTEGER("UID", pw->pw_uid),
+                                     SD_JSON_BUILD_PAIR_INTEGER("GID", pw->pw_gid),
+                                     SD_JSON_BUILD_PAIR_STRING("GECOS", pw->pw_gecos),
+                                     SD_JSON_BUILD_PAIR_STRING("dir", pw->pw_dir),
+                                     SD_JSON_BUILD_PAIR_STRING("shell", pw->pw_shell));
+  if (r < 0)
+    {
+      fprintf(stderr, "Error building passwd data: %s\n", strerror(-r));
+      return -r;
+    }
+
+  if (sp)
+    {
+      r = sd_json_variant_merge_objectbo(&shadow,
+                                         SD_JSON_BUILD_PAIR_STRING("name", sp->sp_namp),
+                                         SD_JSON_BUILD_PAIR_STRING("passwd", sp->sp_pwdp),
+                                         SD_JSON_BUILD_PAIR_INTEGER("lstchg", sp->sp_lstchg),
+                                         SD_JSON_BUILD_PAIR_INTEGER("min", sp->sp_min),
+                                         SD_JSON_BUILD_PAIR_INTEGER("max", sp->sp_max),
+                                         SD_JSON_BUILD_PAIR_INTEGER("warn", sp->sp_warn),
+                                         SD_JSON_BUILD_PAIR_INTEGER("inact", sp->sp_inact),
+                                         SD_JSON_BUILD_PAIR_INTEGER("expire", sp->sp_expire),
+                                         SD_JSON_BUILD_PAIR_INTEGER("flag", sp->sp_flag));
+      if (r < 0)
+        {
+	  fprintf(stderr, "Error building shadow data: %s\n", strerror(-r));
+	  return -r;
+	}
+    }
+
+  r = sd_json_variant_merge_objectbo(&params, SD_JSON_BUILD_PAIR_VARIANT("passwd", passwd));
+  if (r >= 0 && shadow)
+    r = sd_json_variant_merge_objectbo(&params, SD_JSON_BUILD_PAIR_VARIANT("shadow", shadow));
+  if (r < 0)
+    {
+      fprintf(stderr, "JSON merge result object failed: %s", strerror(-r));
+      return -r;
+    }
+
+  const char *error_id = NULL;
+  r = sd_varlink_call(link, "org.openSUSE.pwupd.UpdatePasswdShadow", params, &result, &error_id);
+  if (r < 0)
+    {
+      fprintf(stderr, "Failed to call UpdatePasswdShadow method: %s\n", strerror(-r));
+      return r;
+    }
+
+  /* dispatch before checking error_id, we may need the result for the error
+     message */
+  r = sd_json_dispatch(result, dispatch_table, SD_JSON_ALLOW_EXTENSIONS, &p);
+  if (r < 0)
+    {
+      fprintf(stderr, "Failed to parse JSON answer: %s\n", strerror(-r));
+      return r;
+    }
+
+  if (error_id && strlen(error_id) > 0)
+    {
+      if (p.error)
+	fprintf(stderr, "Error updating account information:\n%s\n", p.error);
+      else
+	fprintf(stderr, "Error updating account information:\n%s\n", error_id);
+      return -EIO;
+    }
+
+  return 0;
+}
 
 int
-main(int argc _unused_, char **argv _unused_)
+main(int argc, char **argv)
 {
-  int flags = 0;
+  const char *user = NULL;
+  int args = 0;
+  int pam_flags = 0;
 
   while (1)
     {
@@ -51,20 +205,24 @@ main(int argc _unused_, char **argv _unused_)
       int option_index = 0;
       static struct option long_options[] =
         {
+	  {"delete",      no_argument,       NULL, 'd' },
           {"keep-tokens", no_argument,       NULL, 'k' },
 	  {"version",     no_argument,       NULL, 'v' },
           {"help",        no_argument,       NULL, 'h' },
           {NULL,          0,                 NULL, '\0'}
         };
 
-      c = getopt_long (argc, argv, "khv",
+      c = getopt_long (argc, argv, "dkhv",
                        long_options, &option_index);
       if (c == (-1))
         break;
       switch (c)
         {
+	case 'd':
+	  args |= ARG_DELETE_PASSWORD;
+	  break;
 	case 'k':
-	  flags |= PAM_CHANGE_EXPIRED_AUTHTOK;
+	  pam_flags |= PAM_CHANGE_EXPIRED_AUTHTOK;
 	  break;
         case 'h':
           print_help();
@@ -87,32 +245,35 @@ main(int argc _unused_, char **argv _unused_)
       print_error();
       return EINVAL;
     }
+
+  if (argc == 1)
+    user = argv[0];
+  else
+    {
+      struct passwd *pw = getpwuid(getuid());
+      if (pw == NULL)
+	{
+	  fprintf(stderr, "User (%u) not found!\n", getuid());
+	  return ENOENT;
+	}
+
+      user = strdupa(pw->pw_name);
+      if (user == NULL)
+	{
+	  fprintf(stderr, "Out of memory!\n");
+	  return ENOMEM;
+	}
+    }
+
+  if (args)
+    return modify_account(user, args);
   else
     {
       _cleanup_(sd_varlink_unrefp) sd_varlink *link = NULL;
       _cleanup_(sd_json_variant_unrefp) sd_json_variant *params = NULL;
-      const char *user = NULL;
+      _cleanup_free_ char *error = NULL;
       int r;
 
-      if (argc == 1)
-        user = argv[0];
-      else
-        {
-          struct passwd *pw = getpwuid(getuid());
-          if (pw == NULL)
-            {
-              fprintf(stderr, "User (%u) not found!\n", getuid());
-              return ENOENT;
-            }
-          user = strdupa(pw->pw_name);
-          if (user == NULL)
-            {
-              fprintf(stderr, "Out of memory!\n");
-              return ENOMEM;
-            }
-        }
-
-      _cleanup_free_ char *error = NULL;
       r = connect_to_pwupdd(&link, _VARLINK_PWUPD_SOCKET, &error);
       if (r < 0)
 	{
@@ -131,17 +292,16 @@ main(int argc _unused_, char **argv _unused_)
 	  return -r;
 	}
 
-      if (flags)
+      if (pam_flags)
 	{
 	  r = sd_json_variant_merge_objectbo(&params,
-					     SD_JSON_BUILD_PAIR_INTEGER("flags", flags));
+					     SD_JSON_BUILD_PAIR_INTEGER("flags", pam_flags));
 	  if (r < 0)
 	    {
 	      fprintf(stderr, "Failed to build param list: %s\n", strerror(-r));
 	      return -r;
 	    }
 	}
-
 
       r = sd_varlink_bind_reply(link, reply_callback);
       if (r < 0)
