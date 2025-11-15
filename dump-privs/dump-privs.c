3ff53684d02f3e4a531e41862c4fb8e082c98737
Thorsten Kukuk <kukuk@suse.com>
2025-11-15 21:42:52 +0100

    dump-privs: add option to print environment

--- dump-privs/dump-privs.c
+++ dump-privs/dump-privs.c
@@ -1,15 +1,81 @@
 
-#define WITH_SELINUX 1
+#define _GNU_SOURCE 1
+
+#include "config.h"
 
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <sys/prctl.h>
 #ifdef WITH_SELINUX
 #include <selinux/selinux.h>
 #endif
+
+static void
+print_usage(FILE *stream)
+{
+  fprintf(stream, "Usage: dump-privs [options]\n");
+}
+
+static void
+print_help(void)
+{
+  fprintf(stdout, "dump-privs - dump process privileges\n\n");
+
+  print_usage(stdout);
+
+  fputs("  -e, --enivronment      Print enviornment variables\n", stdout);
+  fputs("  -w, --wait             Wait for <enter> before exit\n", stdout);
+  fputs("  -h, --help             Give this help list\n", stdout);
+  fputs("  -v, --version          Print program version\n", stdout);
+}
+
+static void
+print_error(void)
+{
+  fprintf (stderr, "Try `dump-privs --help' for more information.\n");
+}
+
+/* conpare function for qsort */
+static int
+compare_str(const void *a, const void *b)
+{
+  return strcmp(*(const char **)a, *(const char **)b);
+}
+
+static int
+agetgroups(int *ngids, gid_t **res)
+{
+  gid_t *gids;
+  int n;
+
+  *ngids = 0;
+  *res = NULL;
+
+  n = getgroups(0, NULL);
+  if (n == -1)
+    return -errno;
+
+  gids = calloc(n, sizeof(gid_t));
+  if (gids == NULL)
+    return -ENOMEM;
+
+  n = getgroups(n, gids);
+  if (n == -1)
+    {
+      int r = errno;
+
+      free(gids);
+      return -r;
+    }
+
+  *ngids = n;
+  *res = gids;
+  return 0;
+}
 
 static const char *
 selinux_status(void)
@@ -41,11 +107,67 @@ selinux_status(void)
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
+  bool wait = false;
+  bool printenv = false;
+  int ngids = 0;
+  gid_t *gids = NULL;
   char *secon = NULL;
   int no_new_privs = prctl(PR_GET_NO_NEW_PRIVS, 0, 0, 0, 0);
   const char *sestatus = selinux_status();
+  int r;
+
+  while (1)
+    {
+      int c;
+      int option_index = 0;
+      static struct option long_options[] =
+        {
+	  {"environment", no_argument,       NULL, 'e' },
+          {"help",        no_argument,       NULL, 'h' },
+          {"version",     no_argument,       NULL, 'v' },
+          {"wait",        no_argument,       NULL, 'w' },
+          {NULL,          0,                 NULL, '\0'}
+        };
+
+      c = getopt_long (argc, argv, "ehvw",
+                       long_options, &option_index);
+      if (c == (-1))
+        break;
+      switch (c)
+        {
+	case 'e':
+	  printenv = true;
+	  break;
+        case 'h':
+          print_help();
+          return 0;
+        case 'v':
+          printf("dump-privs (%s) %s\n", PACKAGE, VERSION);
+          return 0;
+        case 'w':
+	  wait = true;
+          break;
+        default:
+          print_error();
+          return 1;
+        }
+    }
+
+  argc -= optind;
+  argv += optind;
+
+  if (argc > 0)
+    {
+      fprintf(stderr, "dump-privs: Too many arguments.\n");
+      print_error();
+      return EINVAL;
+    }
+
+  r = agetgroups(&ngids, &gids);
+  if (r != 0)
+    fprintf(stderr, "getgroups() failed: %s\n", strerror(-r));
 
   printf("🔎 Process Security Information\n");
   printf("-------------------------------\n");
@@ -53,6 +175,14 @@ main(void)
   printf("Effective UID:     %d\n", geteuid());
   printf("Real GID:          %d\n", getgid());
   printf("Effective GID:     %d\n", getegid());
+  printf("Group memberships:");
+  for (int i = 0; i < ngids; i++)
+    {
+      if (i != 0)
+	putchar(',');
+      printf(" %jd", (intmax_t) gids[i]);
+    }
+  putchar('\n');
   printf("SELinux Status:    %s\n", sestatus);
   if (getcon(&secon) == 0)
     {
@@ -60,10 +190,30 @@ main(void)
       freecon(secon); /* Free the memory allocated by getcon() */
     }
   else
-    printf("SELinux Context:   %s\n", strerror(errno));
+    fprintf(stderr, "SELinux Context:   %s\n", strerror(errno));
   printf("NoNewPrivs Status: %s\n", no_new_privs==0?"off":"on");
 
-  getchar();
+  if (printenv)
+    {
+      int count = 0;
+      char **envp = environ;
+      while (*envp != NULL)
+	{
+	  count++;
+	  envp++;
+	}
+      qsort(environ, count, sizeof(char *), compare_str);
+      envp = environ;
+      fputs("Environement variables:\n", stdout);
+      while (*envp != NULL)
+	{
+	  printf("%s\n", *envp);
+	  envp++;
+	}
+    }
+
+  if (wait)
+    getchar();
 
   return 0;
 }
